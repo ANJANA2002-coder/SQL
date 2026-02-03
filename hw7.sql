f2937b20979b7abe285ba2c9e1cb154d8e25c023
ANJANA2002-coder <gnairanjana1@gmail.com>
2026-02-03 19:38:21 +0530

    hw create dastabase and tables

---
+++ hw7.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE bookstore_db;
+USE bookstore_db;
+CREATE TABLE authors(author_id INT NOT NULL AUTO_INCREMENT,name TEXT(38)NOT NULL ,country TEXT(45) NOT NULL,PRIMARY KEY(author_id));
